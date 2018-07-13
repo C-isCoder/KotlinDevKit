8a30102a573186b4ce91c0852a85635baeedff8c
orzlllll@qq.com <congxiaodan>
2018-07-13 10:28:02 +0800

    add HanziToPinyin  and strings

--- kotlin-kit/src/main/java/com/baichang/android/kotlin/kit/widget/ShareDialog.kt
+++ kotlin-kit/src/main/java/com/baichang/android/kotlin/kit/widget/ShareDialog.kt
@@ -1,58 +1,55 @@
 package com.baichang.android.kotlin.kit.widget
 
-import android.annotation.SuppressLint
-import android.os.Bundle
+import android.content.Context
+import android.support.design.widget.BottomSheetDialog
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
 import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Button
 import com.baichang.android.kotlin.kit.R
-import kotlinx.android.synthetic.main.dialog_share_util.btnCancel
-import kotlinx.android.synthetic.main.dialog_share_util.rvList
+import com.baichang.android.kotlin.kit.http.HttpDialog.dismiss
+import com.baichang.android.kotlin.kit.widget.ShareDialog.ShareAdapter.Holder
+import kotlinx.android.synthetic.main.item_share.view.ivLogo
+import kotlinx.android.synthetic.main.item_share.view.tvName
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
-@SuppressLint("ValidFragment")
-class ShareDialog(val action: (type: Int) -> Unit) : FullScreenDialogFragment() {
+class ShareDialog {
 
   companion object {
     const val WECHAT = 0  // 微信
     const val CIRCLE = 1  // 朋友圈
+    fun show(
+      context: Context,
+      action: (type: Int) -> Unit
+    ) {
+      val dialog = BottomSheetDialog(context)
+      val contentView = LayoutInflater.from(context)
+          .inflate(R.layout.dialog_share_util, null)
+      val list = contentView.findViewById<RecyclerView>(R.id.rvList)
+      val btnCancel = contentView.findViewById<Button>(R.id.btnCancel)
+      list.adapter = ShareAdapter {
+        action.invoke(it)
+        dialog.dismiss()
+      }
+      btnCancel.onClick { dialog.dismiss() }
+      dialog.setContentView(contentView)
+      dialog.show()
+    }
   }
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.dialog_share_util, container, false)
-  }
-
-  override fun onViewCreated(
-    view: View,
-    savedInstanceState: Bundle?
-  ) {
-    super.onViewCreated(view, savedInstanceState)
-    rvList.adapter = ShareAdapter()
-    btnCancel.setOnClickListener { dismiss() }
-  }
-
-  inner class ShareAdapter : RecyclerView.Adapter<Holder>() {
+  class ShareAdapter(val action: (type: Int) -> Unit) : Adapter<Holder>() {
 
     val names = listOf("微信", "朋友圈")
-    val images = listOf(
-        R.mipmap.we_chat,
-        R.mipmap.icon_moments
-    )
+    val images = listOf(R.mipmap.we_chat, R.mipmap.icon_moments)
 
     override fun onCreateViewHolder(
       parent: ViewGroup,
       viewType: Int
     ): Holder = Holder(
-        LayoutInflater.from(parent.context).inflate(
-            R.layout.item_share, parent, false
-        )
+        LayoutInflater.from(parent.context).inflate(R.layout.item_share, parent, false)
     )
 
     override fun getItemCount(): Int = names.size
@@ -61,17 +58,16 @@ class ShareDialog(val action: (type: Int) -> Unit) : FullScreenDialogFragment() 
       holder: Holder,
       position: Int
     ) {
-      holder.ivLogo.setImageResource(images[position])
-      holder.tvName.text = names[position]
-      holder.itemView.setOnClickListener {
+      holder.itemView.ivLogo.setImageResource(images[position])
+      holder.itemView.tvName.text = names[position]
+      holder.itemView.onClick {
         action.invoke(position)
         dismiss()
       }
     }
+
+    inner class Holder(view: View) : ViewHolder(view)
+
   }
 
-  inner class Holder(view: View) : ViewHolder(view) {
-    val ivLogo: ImageView = view.findViewById(R.id.ivLogo)
-    val tvName: TextView = view.findViewById(R.id.tvName)
-  }
 }
