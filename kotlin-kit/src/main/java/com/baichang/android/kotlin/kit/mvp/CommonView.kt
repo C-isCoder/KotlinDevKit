e47dca91c1f1244b078e4506f3a04667202f9c5b
orzlllll@qq.com <congxiaodan>
2018-08-07 17:42:10 +0800

    CommonView add more interface method

--- kotlin-kit/src/main/java/com/baichang/android/kotlin/kit/mvp/CommonView.kt
+++ kotlin-kit/src/main/java/com/baichang/android/kotlin/kit/mvp/CommonView.kt
@@ -2,8 +2,8 @@ package com.baichang.android.kotlin.kit.mvp
 
 interface CommonView<T> {
   var presenter: T
-  //fun showMessage(msgId: Int)
-  //fun showMessage(msg: String)
-  //fun showProgress()
-  //fun hideProgress()
+  fun showMessage(msgId: Int)
+  fun showMessage(msg: String)
+  fun showProgress()
+  fun hideProgress()
 }
